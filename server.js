7a18426a1a82725b20b57c2b7d8d1d7f05e6766b
Islande Silva <islandefs@gmail.com>
2026-02-04 12:20:19 -0300

    Fix: corrigindo nome do repositório para islandex

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('ScanBR: Sistema Online - OK\n');
+    res.end('ScanBR: Sistema Online - OK - Update v1.0.0.1\n');
 });
 
 const PORT = process.env.PORT || 3000;
